3e33d6c06415ff3ae2114f64448ea040696e5bc3
Tim Berchem <tim.berchem@gmail.com>
2024-09-26 14:01:26 +0200

    feat: add tests

---
+++ test_ihk_notenberechnung.py
@@ -0,0 +1,56 @@
+import pytest
+
+test_data_prozente__positive_test = [
+    (100, 100, 100),
+    (0, 100, 0),
+    (0, 6, 0)
+]
+
+test_data_prozente__negative_test = [
+    (-1, 100, 'ValueError'),
+    (0, 5, 'ValueError'),
+    (11, 10, 'ValueError'),
+    ('text', 100, 'TypeError'),
+    (10, 'text', 'TypeError')
+]
+
+test_note__positive_test = [
+    (100, 'sehr gut')
+    (92, 'sehr gut')
+    (91, 'gut')
+    (81, 'gut')
+    (80, 'befriedigend')
+    (67, 'befriedigend')
+    (66, 'ausreichend')
+    (50, 'ausreichend')
+    (49, 'mangelhaft')
+    (30, 'mangelhaft')
+    (29, 'ungenügend')
+    (0, 'ungenügend')
+]
+
+test_note__negative_test = [
+    (-1, 'ValueError')
+    (101, 'ValueError')
+    ('text', 'TypeError')
+]
+
+@pytest.mark.parametrize('punkte, max_punkte, expected', test_data_prozente__positive_test)
+def test_berechne_prozente__positive_test(punkte, max_punkte, expected):
+    percentage = berechne_prozente(punkte, max_punkte)
+    assert percentage == expected
+
+@pytest.mark.parametrize('punkte, max_punkte, expected', test_data_prozente__negative_test)
+def test_berechne_prozente__negative_test(punkte, max_punkte, expected):
+    percentage = berechne_prozente(punkte, max_punkte)
+    assert percentage == expected
+
+@pytest.mark.parametrize('prozent_werte, expected', test_note__positive_test)
+def test_note__positive_test(prozent_werte, expected):
+    note = berechne_prozente(prozent_werte)
+    assert note == expected
+
+@pytest.mark.parametrize('prozent_werte, expected', test_note__negative_test)
+def test_note__positive_test(prozent_werte, expected):
+    note = berechne_prozente(prozent_werte)
+    assert note == expected
